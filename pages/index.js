a2ae08161d6dc95b1ca0537dab23f6d869bd1a20
Anton Ampilov <anton_ampilov@mail.ru>
2022-01-15 16:31:08 +0300

    starting commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>How working tailwind CSS with styled-components</h1>
+    </div>
+  )
+}

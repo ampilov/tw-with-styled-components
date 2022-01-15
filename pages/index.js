27f1e37165360cb2cdbe42576fb2c16ef94f4f81
Anton Ampilov <anton_ampilov@mail.ru>
2022-01-15 23:33:04 +0300

    it is working

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import styled from 'styled-components';
+import tw from 'twin.macro';
 
 export default function Home() {
   return (
     <Wrapper>
       <h1>How working tailwind CSS with styled-components</h1>
+      <ButtonTW>Send</ButtonTW>
+      <ButtonTW>Send</ButtonTW>
+      <ButtonTW>Send</ButtonTW>
+      <ButtonTW>Send</ButtonTW>
+      <ButtonTW>Send</ButtonTW>
+      <ButtonTW>Send</ButtonTW>
+      <ButtonTW>Send</ButtonTW>
+      <ButtonTW>Send</ButtonTW>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
-flex: 1;
-`;
+const Wrapper = tw.div`
+
+`
+
+const ButtonTW = tw.button`
+bg-black
+font-bold
+text-red-500
+`

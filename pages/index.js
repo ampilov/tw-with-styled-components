dc0d47dea11c808f6638c627e9a9d64c00d3f047
Anton Ampilov <anton_ampilov@mail.ru>
2022-01-15 17:00:33 +0300

    was install and configuring tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-black'>
       <h1>How working tailwind CSS with styled-components</h1>
     </div>
   )
